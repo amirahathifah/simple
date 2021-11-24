644988abe6de87e8a100874ce620ba0035c9bd40
amirahathifah <azaharmyra95@gmail.com>
2021-11-24 11:46:13 +0800

    first commit

---
+++ dashboard.php
@@ -0,0 +1,54 @@
+<style>
+.rounded-xxl{
+    border-radius: 30px;
+}
+</style>
+<div class="card card-custom card-stretch card-stretch-half gutter-b">
+    <!--begin::Body-->
+    <div class="card-body row">
+        <div class="col"></div>
+        <div class="col">
+            <div class="text-center bg-primary border p-3">
+                <img src="image/mysejahtera.png" class="border rounded-xxl m-10">
+                <p class="h3 text-white font-weight-bolder">NOR AMIRAH ATHIFAH BT AZAHAR</p>
+                <p class="h6 text-white">951123-11-5072</p>
+            </div>
+            <div class="text-left bg-light border p-6">
+                <div class="row bg-success rounded-lg p-6">
+                    <div class="col-2">
+                        <i class="fas fa-virus icon-4x text-white"></i>
+                    </div>
+                    <div class="col">
+                        <p class="text-white">COVID-19 Risk Status</p>
+                        <p class="h5 text-white font-weight-bolder">Low Risk No Symptom</p>
+                    </div>
+                </div>
+                <div class="row bg-warning rounded-lg p-6 mt-6 mb-6">
+                    <div class="col-2">
+                        <i class="fas fa-syringe icon-4x text-white"></i>
+                    </div>
+                    <div class="col">
+                        <p class="text-white">COVID-19 Vaccination Status</p>
+                        <p class="h5 text-white font-weight-bolder">Fully Vaccinated</p>
+                    </div>
+                </div>
+                <div class="row bg-white border rounded-lg p-6">
+                    <div class="col-4">
+                        <img src="image/home.png" class="img-thumbnail rounded-circle p-4 bg-light">
+                    </div>
+                    <div class="col">
+                        <p class="text-primary text-right">history</p>
+                        <p>Last Check-in</p>
+                        <p>Nov 24,2021, 8:12:47 AM</p>
+                        <button class="btn btn-outline-primary btn-block">Check-Out</button>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center bg-white border p-3">
+                <button class="btn btn-primary btn-block">Check-In</button>
+            </div>
+        </div>
+        <div class="col"></div>
+    </div>
+    <!--end::Body-->
+</div>
